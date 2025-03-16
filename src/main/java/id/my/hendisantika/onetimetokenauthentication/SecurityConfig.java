d9b990667e839566e3d1be81306085adceeeea8d
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 11:07:40 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/onetimetokenauthentication/SecurityConfig.java
@@ -0,0 +1,46 @@
+package id.my.hendisantika.onetimetokenauthentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-onetimetoken-authentication
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/03/25
+ * Time: 11.07
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/ott/sent").permitAll()
+                        .requestMatchers("/login/ott").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .oneTimeTokenLogin(Customizer.withDefaults())
+                .build();
+    }
+
+
+    @Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        var dan = User.withUsername("dvega").password("{noop}password").build();
+        return new InMemoryUserDetailsManager(dan);
+    }
+}
