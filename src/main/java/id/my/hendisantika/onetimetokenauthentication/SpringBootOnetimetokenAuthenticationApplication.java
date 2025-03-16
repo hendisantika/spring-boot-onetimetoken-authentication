5856fa7cda23aa16c66c953a67be642e6a479dd4
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 11:02:03 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/onetimetokenauthentication/SpringBootOnetimetokenAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.onetimetokenauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnetimetokenAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOnetimetokenAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/onetimetokenauthentication/SpringBootOnetimetokenAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.onetimetokenauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOnetimetokenAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
