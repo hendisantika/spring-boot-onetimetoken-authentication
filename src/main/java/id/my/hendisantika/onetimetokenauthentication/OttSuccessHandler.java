266eecd15572ad6863203a1244dc6e9ca83a75b0
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 11:06:51 +0700

    feat: Add handle in OttSuccessHandler🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/onetimetokenauthentication/OttSuccessHandler.java
+++ src/main/java/id/my/hendisantika/onetimetokenauthentication/OttSuccessHandler.java
@@ -1,10 +1,18 @@
 package id.my.hendisantika.onetimetokenauthentication;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.ott.OneTimeToken;
 import org.springframework.security.web.authentication.ott.OneTimeTokenGenerationSuccessHandler;
 import org.springframework.security.web.authentication.ott.RedirectOneTimeTokenGenerationSuccessHandler;
+import org.springframework.security.web.util.UrlUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +34,41 @@ public class OttSuccessHandler implements OneTimeTokenGenerationSuccessHandler {
 
     public OttSuccessHandler(EmailService emailService) {
         this.emailService = emailService;
+    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, OneTimeToken oneTimeToken) throws IOException, ServletException {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(UrlUtils.buildFullRequestUrl(request))
+                .replacePath(request.getContextPath())
+                .replaceQuery(null)
+                .fragment(null)
+                .path("/login/ott")
+                .queryParam("token", oneTimeToken.getTokenValue());
+
+        String magicLink = builder.toUriString();
+
+        // send email (JavaMail, SendGrid) or SMS
+        System.out.println("Magic Link: " + magicLink);
+
+        var body = """
+                Hello, from Spring Security! 
+                Below you will find your magic link to login to our super secret application!
+                
+                %s
+                """.formatted(magicLink);
+
+        try {
+            var sendTo = oneTimeToken.getUsername();
+            log.info("Sending One Time Token to username: {}", sendTo);
+            emailService.sendEmail(
+                    getEmail(sendTo),
+                    "One Time Token Login",
+                    body
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.redirectHandler.handle(request, response, oneTimeToken);
     }
 }
