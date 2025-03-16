3322436c72605e1b34f2b998501d30381f22983a
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 11:05:53 +0700

    feat: Add sendEmail in EmailService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/onetimetokenauthentication/EmailService.java
+++ src/main/java/id/my/hendisantika/onetimetokenauthentication/EmailService.java
@@ -3,6 +3,8 @@ package id.my.hendisantika.onetimetokenauthentication;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-onetimetoken-authentication
@@ -19,4 +21,29 @@ public class EmailService {
 
     @Value("${sendgrid.api-key}")
     private String sendgridApiKey;
+
+    public void sendEmail(String to, String subject, String content) throws IOException {
+        Email from = new Email("danvega@gmail.com");
+        Email toEmail = new Email(to);
+        Content emailContent = new Content("text/plain", content);
+        Mail mail = new Mail(from, subject, toEmail, emailContent);
+
+        // configure SendGrid client
+        SendGrid sg = new SendGrid(sendgridApiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+
+            Response response = sg.api(request);
+
+            System.out.println("Status Code: " + response.getStatusCode());
+            System.out.println("Response Body " + response.getBody());
+            System.out.println("Response Headers " + response.getHeaders());
+        } catch (IOException ex) {
+            throw new IOException("Failed to send email: " + ex.getMessage());
+        }
+    }
 }
